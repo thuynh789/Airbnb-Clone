ceb1fef05aaab6c870b01d5a1503ccf5d6476f66
tiana3113 <t.huynh3113@gmail.com>
2022-12-20 17:32:26 -0800

    Spots seeds

--- backend/db/seeders/20221220205423-demo-spotimages.js
+++ backend/db/seeders/20221220205423-demo-spotimages.js
@@ -1,5 +1,12 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'SpotImages';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -12,6 +19,28 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+     return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: '',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: '',
+        preview: true
+      }
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
