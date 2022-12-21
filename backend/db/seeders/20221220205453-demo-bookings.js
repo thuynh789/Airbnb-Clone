7a5568c23c83c589732afb5c1e842893a57aaa1e
tiana3113 <t.huynh3113@gmail.com>
2022-12-20 17:55:09 -0800

    booking seeders

--- backend/db/seeders/20221220205453-demo-bookings.js
+++ backend/db/seeders/20221220205453-demo-bookings.js
@@ -1,5 +1,12 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'Bookings';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -12,6 +19,38 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+     return queryInterface.bulkInsert(options, [
+      {
+        spotId: 4,
+        userId: 1,
+        startDate: '2023-02-01',
+        endDate: '2023-02-05'
+      },
+      {
+        spotId: 3,
+        userId: 2,
+        startDate: '2023-03-15',
+        endDate: '2023-03-20'
+      },
+      {
+        spotId: 1,
+        userId: 3,
+        startDate: '2023-04-01',
+        endDate: '2023-04-09'
+      },
+      {
+        spotId: 2,
+        userId: 3,
+        startDate: '2023-05-21',
+        endDate: '2023-05-24'
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        startDate: '2023-11-17',
+        endDate: '2023-11-23'
+      },
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +60,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     return queryInterface.bulkDelete(options, {}, {});
   }
 };
