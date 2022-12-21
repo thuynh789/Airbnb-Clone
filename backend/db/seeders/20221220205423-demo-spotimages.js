a063b007794646b0dd24f0ab5b91b1dd2d0f3824
tiana3113 <t.huynh3113@gmail.com>
2022-12-20 17:42:13 -0800

    spotimage seeds

--- backend/db/seeders/20221220205423-demo-spotimages.js
+++ backend/db/seeders/20221220205423-demo-spotimages.js
@@ -22,22 +22,22 @@ module.exports = {
      return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: '',
+        url: 'https://i.pinimg.com/originals/ba/53/89/ba5389484514c0aa232062040b7dd40e.jpg',
         preview: true
       },
       {
         spotId: 2,
-        url: '',
+        url: 'https://www.shareoregon.com/things-to-do/system/images/237424/big/pionerplace3.jpg?1501559771',
         preview: true
       },
       {
         spotId: 3,
-        url: '',
+        url: 'https://cdn.vox-cdn.com/thumbor/Mte_wxG4cKQw9PBh5cuMWgiw8Vw=/0x0:920x613/1200x800/filters:focal(581x104:727x250)/cdn.vox-cdn.com/uploads/chorus_image/image/63257148/Little_Big_Burger_.0.jpg',
         preview: true
       },
       {
         spotId: 4,
-        url: '',
+        url: 'https://images.squarespace-cdn.com/content/v1/52310be0e4b03288c7519651/1378987369897-3NKMMFW76XV5S58K19AT/IMG_5676_1.jpg?format=2500w',
         preview: true
       }
     ], {});
@@ -50,5 +50,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     return queryInterface.bulkDelete(options, {}, {});
   }
 };
