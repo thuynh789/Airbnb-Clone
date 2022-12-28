47a993f1aef6b00d0ec55340f10e5859888d9f58
tiana3113 <t.huynh3113@gmail.com>
2022-12-27 20:23:43 -0800

    feature 0 updates, added login validators and error messages

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -37,7 +37,7 @@ const validateSignup = [
 
 // Sign up
 router.post(
-  '/',
+'/',
   validateSignup,
   async (req, res) => {
     const { email, password, username, firstName, lastName } = req.body;
